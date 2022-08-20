4b11ed29fab4fbef12f7d374fad16058af903335
borisimus <borisimusb@yandex.ru>
2022-08-20 08:51:58 +0500

    Add Random

--- Examples_003_Sum/Program.cs
+++ Examples_003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 10;
+﻿int numberA = new Random().Next(1,100); //1 2 3 4... 99
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,100); //1 2 3 4... 99
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
